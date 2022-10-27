73bc906507284015ba5f80d19c8cb3376dc9673a
mziatvaradze <113588110+mziatvaradze@users.noreply.github.com>
2022-10-27 16:23:18 +0400

    14

---
+++ app.js
@@ -0,0 +1,19 @@
+const first=[1, 2, 3, 4, 5];
+const value=( first[0]+first[1]+first[2]+first[3]+first[4] ) /5;
+
+const second={nino:23, mari:34, ani:47, tako:18, qeti:28};
+
+const info={
+    firstName:"kira",
+    lastName:"light",
+    age:28,
+    phoneNumbers:"987654321",
+    address:["tbilisi","saburtalo"],
+    zipcode:5865
+};
+const text= "User name is "+ info.firstName + " user age is " + info.age +"user address is "+ info.address; 
+
+
+/* console.table(
+    text
+); */
